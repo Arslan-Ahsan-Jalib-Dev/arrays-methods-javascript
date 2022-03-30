16fc7a54a82c1925a5780e8ad81ddf42a246a6e3
Arslan Ahsan <arslanahsanjalib050@gmail.com>
2022-03-30 15:36:08 +0500

    code added

---
+++ script.js
@@ -0,0 +1,22 @@
+//***  Example of arrays ForEach Method in javascript 
+var myarray=[9,10,11,12,13];
+myarray[20]=100;
+console.log(myarray);
+//forEach takes the callback function as an argument and this callback function takes three arguments current   item its index and array which is to be processed
+
+myarray.forEach(display);              
+
+function display(item,index,arry){
+//    console.log(index + " " + item + " ");
+}
+//convert it in to arrow function
+ myarray.forEach((item,index)=>{
+//     console.log(index + " " + item);
+ })
+
+// 1) Sum of the array
+var result=0;
+myarray.map(item=>{
+    result=item + result;
+})
+//console.log(result);
